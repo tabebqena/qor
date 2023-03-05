a484bd00fc506c94b90f231caa46d42da47bcaa5
tabebqena@gmail.com <tabebqena@gmail.com>
2023-03-05 21:59:22 +0300

    clean requirements

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ setup(
     version="0.0.1",
     description="python framework for kore server",
     packages=["src"],
-    install_requires=[
-        "Click",
-    ],
     entry_points={
         "console_scripts": [
             "yourscript = yourscript:cli",
